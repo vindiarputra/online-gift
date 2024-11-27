aa99713e066f1960fadfe137dfb2343d4439d7f5
vindiarputra <vindiarptr@gmail.com>
2024-11-28 01:00:19 +0700

    Add clickable logo component with routing to homepage

--- src/components/Organisms/Navbar.tsx
+++ src/components/Organisms/Navbar.tsx
@@ -44,7 +44,8 @@ export default function Navbar() {
 			<div className="max-w-6xl mx-auto">
 				<div className="flex justify-between items-center">
 					{/* Logo */}
-					<div className="flex items-center">
+					
+					<div className="flex items-center" onClick={() => router.push("/")}>
 						<Image src="/images/logo/OG_.svg" alt="Logo" width={60} height={50} />
 					</div>
 
